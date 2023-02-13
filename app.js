cbc85c4f82bbbb5401e5953014d7e24e6604a027
Iswanto25 <agung_iswanto@outlook.co.id>
2023-02-14 03:20:25 +0700

    add-table

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var atletRouter = require('./routes/atlet');
+var pelatihRouter = require('./routes/pelatih');
+var vanueRouter = require('./routes/vanue');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/atlet', atletRouter);
+app.use('/vanue', vanueRouter);
+app.use('/coach', pelatihRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
